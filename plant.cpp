90a69d179c8452d36f14f8cfdc41ab582a7fc75b
laletinivan <laletinivan@users.noreply.github.com>
2021-03-12 19:49:28 +0700

    NewFuncCons

--- plant.cpp
+++ plant.cpp
@@ -9,19 +9,30 @@ namespace plants {
 	tree* InTree(ifstream& ist);
 	void Out(bush& r, ofstream& ofst);
 	void Out(tree& t, ofstream& ofst);
+	// Количество согласных букв
+	int Consanants(plant& p) {
+		string vowel = "AEIOUaeiou";
+		int count = 0;
+		for (int i = 0; i < p.name.length(); i++) {
+			if (vowel.find(p.name[i]) == string::npos) {
+				count++;
+			}
+		}
+		return count;
+	}
 	// Ввод параметров обобщенной фигуры из файла
 	plant* InPlant(ifstream& ifst){
 		plant* pl = new plant;
 		int k;
 		ifst >> k;
+		ifst >> pl->name;
+		pl->cons = Consanants(*pl);
 		switch (k) {
 		case 1:
-			ifst >> pl->name;
 			pl->k = plant::key::BUSH;
 			pl->data = (void*)InBush(ifst);
 			return pl;
 		case 2:
-			ifst >> pl->name;
 			pl->k = plant::key::TREE;
 			pl->data = (void*)InTree(ifst);
 			return pl;
@@ -31,7 +42,8 @@ namespace plants {
 	}
 	// Вывод параметров текущей фигуры в поток
 	void Out(plant& s, ofstream& ofst) {
-		ofst << "Name is " << s.name << " ";
+		ofst << "Name is " << s.name << ", ";
+		ofst << "number of consonants in name = " << s.cons << ", ";
 		switch (s.k) {
 		case plant::key::BUSH:
 			Out(*(bush*)s.data, ofst);
--- plant_atd.h
+++ plant_atd.h
@@ -7,6 +7,7 @@ namespace plants {
 	// структура, обобщающая все имеющиеся фигуры
 	struct plant {
 		std::string name;
+		int cons;
 		// значения ключей для каждой из фигур
 		enum key { BUSH, TREE };
 		key k; // ключ	
